c7d70cc31c3c69375a8443ec3669cdcca9b1c4ac
Dung <dung@rivervolume-lm.corp.yahoo.com>
2014-07-13 00:04:29 -0700

    adds shared twitter app

--- src/com/example/disasternews/DisasterNewsClient.java
+++ src/com/example/disasternews/DisasterNewsClient.java
@@ -11,8 +11,8 @@ public class DisasterNewsClient extends OAuthBaseClient{
 	
 	public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class; // Change this
 	public static final String REST_URL = "https://api.twitter.com/1.1"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = "Jy5Cw2hBY9G3LseERuUupQiGv";       // Change this
-    public static final String REST_CONSUMER_SECRET = "0kMuucuCi9BKsGDgQU3b7FoouSwZHfUikfvy7urcBHQuPCSoLQ"; // Change this
+	public static final String REST_CONSUMER_KEY = "pkGeBJe31xxp05pW6wvcN0NQF";       // Change this
+    public static final String REST_CONSUMER_SECRET = "NSdPuVFH4e7zJRoKcnqrUIb0KOdIAzu4bSeSEPv4VArktnRxEi"; // Change this
 	public static final String REST_CALLBACK_URL = "oauth://cpbasictweets"; // Change this (here and in manifest)
 	
 	public DisasterNewsClient(Context context) {
